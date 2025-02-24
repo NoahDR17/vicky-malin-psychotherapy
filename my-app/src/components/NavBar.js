b1960eba87fc51d3390c46eb24cd2511637d9c50
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2025-02-24 13:46:30 +0000

    Feat: Add services dropdown, and extra functionality for nav dropdown

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -1,31 +1,83 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React, { useState, useRef, useEffect } from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+  const [expanded, setExpanded] = useState(false);
+  const navRef = useRef(null);
+
+  // Close the navbar after a nav link is clicked
+  const handleNavClick = () => setExpanded(false);
+
+  // Close navbar when clicking outside of it
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (navRef.current && !navRef.current.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+    return () => document.removeEventListener('click', handleClickOutside);
+  }, []);
+
   return (
-    <Navbar bg="light" expand="lg">
+    <Navbar
+      ref={navRef}
+      bg="light"
+      expand="lg"
+      expanded={expanded}
+      onToggle={(nextExpanded) => setExpanded(nextExpanded)}
+    >
       <Container>
-        <Navbar.Brand as={Link} to="/">
-        Victoria Malin
-          {/* <img 
-            src="/logo.png" 
-            alt="Logo" 
-            width="100" 
-            height="40"
-            className="d-inline-block align-top"
-          /> */}
+        <Navbar.Brand as={Link} to="/" onClick={handleNavClick}>
+          Victoria Malin
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/about">About Me</Nav.Link>
-            <Nav.Link as={Link} to="/services">Services</Nav.Link>
-            <Nav.Link as={Link} to="/resources">Resources</Nav.Link>
-            <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/" onClick={handleNavClick}>
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/about" onClick={handleNavClick}>
+              About Me
+            </Nav.Link>
+
+            <NavDropdown title="Services" id="services-dropdown">
+              <NavDropdown.Item
+                as={Link}
+                to="/services/In-Person"
+                onClick={handleNavClick}
+              >
+                In-Person
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                as={Link}
+                to="/services/Online"
+                onClick={handleNavClick}
+              >
+                Online
+              </NavDropdown.Item>
+              <NavDropdown.Item
+                as={Link}
+                to="/services/WalkAndTalk"
+                onClick={handleNavClick}
+              >
+                Walk and Talk
+              </NavDropdown.Item>
+            </NavDropdown>
+
+            <Nav.Link as={Link} to="/resources" onClick={handleNavClick}>
+              Resources
+            </Nav.Link>
+            <Nav.Link as={Link} to="/faq" onClick={handleNavClick}>
+              FAQ
+            </Nav.Link>
+            <Nav.Link as={Link} to="/contact" onClick={handleNavClick}>
+              Contact
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
