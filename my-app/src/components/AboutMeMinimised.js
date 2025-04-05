6663430ea66b4a64ecbca2b885833caf74b67a85
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2025-04-05 11:21:27 +0000

    Feat: update about me minimised component

--- my-app/src/components/AboutMeMinimised.js
+++ my-app/src/components/AboutMeMinimised.js
@@ -20,8 +20,8 @@ const AboutMeMinimised = () => {
           />
         </div>
         <p>
-          Thank you for taking the time to look at my website. <br /><br />
-          My name is Victoria Malin. I am a qualified{' '}
+          Welcome and thank you for visiting my website. <br /><br />
+          I’m a qualified and{' '}
           <span
             className="hyperlink"
             onClick={() =>
@@ -33,32 +33,21 @@ const AboutMeMinimised = () => {
           >
             BACP
           </span>{' '}
-          registered counsellor. <br />
-          I have been working as a counsellor since 2022 and set up my private practice in Southport last year.
-          I provide counselling in the Southport and Formby area to Adults on a one-to-one basis.
-          I offer 60 minute sessions for £45 both in-person, and remotely by video or telephone. <br /><br />
-          Choosing a counsellor can feel overwhelming so I also offer a free no obligation
-          20-30 minute session to help you decide if I am the right counsellor for you and to ease any worries you may have about making your choice.
+          registered counsellor and psychotherapist, offering one-to-one therapy to adults in the Southport and Formby areas.
+          <br />
+          I have been working as a psychotherapist since 2022 and opened my private practice in 2023.
+          I offer 60-minute sessions at £50, available both in-person and remotely via video or telephone to suit your needs and preferences.
         </p>
         <p>
-          You can find me on the{' '}
-          <span
-            className="hyperlink"
-            onClick={() =>
-              window.open(
-                'https://www.bacp.co.uk/profile/8b2fbfe2-630e-eb11-a813-000d3ab852db',
-                '_blank'
-              )
-            }
-          >
-            BACP website
-          </span>.{' '}
-          If you would like, you can also{' '}
-          <span className="hyperlink" onClick={() => navigate('/about')}>
-            click here
-          </span>{' '}
-          to learn more about me.
+          Choosing a counsellor can feel overwhelming, and I understand how important it is to find someone who feels right for you. To help with this decision, I offer a free, no-obligation 30-minute session. This allows us to meet, discuss your goals, and see if I’m the right fit for you, without any pressure.
         </p>
+        <p>
+          If you would like to learn more{' '}
+          <span className="hyperlink" onClick={() => navigate('/about')}>
+            about me
+          </span>
+          , please feel free to reach out with any questions or explore my website further to learn more about my approach to therapy, services, and answers to frequently asked questions.
+          </p>
         <p>
           If you have any questions please feel free to get in{' '}
           <span className="hyperlink" onClick={() => navigate('/contact')}>
@@ -72,8 +61,8 @@ const AboutMeMinimised = () => {
       </div>
       <div className="image-section">
         <img
-            src="https://res.cloudinary.com/du7daaai2/image/upload/v1740507722/victoria_ikiloe.jpg"
-            alt="Counsellor"
+          src="https://res.cloudinary.com/du7daaai2/image/upload/v1740507722/victoria_ikiloe.jpg"
+          alt="Counsellor"
         />
       </div>
     </div>
