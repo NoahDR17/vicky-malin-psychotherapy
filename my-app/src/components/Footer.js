2af44fee89731f4dfc8bd62b0442b90d3921bc74
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2025-04-05 12:53:58 +0000

    Feat: remove nav links in footer

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -4,20 +4,11 @@ import '../styles/Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            {/* Row 1: Menu */}
-            <div className="footer-menu">
-                <a href="/">Home</a>
-                <a href="/about">About Me</a>
-                <a href="/services">Services</a>
-                <a href="/resources">Resources</a>
-                <a href="/faq">FAQ</a>
-                <a href="/contact">Contact</a>
-            </div>
-
             {/* Row 2: Location/Contact */}
             <div className="footer-contact">
                 <p>Unit 2, 19 Anchor Street, Southport, PR9 0UT, England</p>
-                <p>Phone: (+44) 07378 382938 <br/> Email: victoriamalin.counselling@gmail.com</p>
+                <p>Phone: (+44) 07378 382938</p>
+                <p> Email: victoriamalin.counselling@gmail.com</p>
             </div>
 
             {/* Row 3: Associations */}
